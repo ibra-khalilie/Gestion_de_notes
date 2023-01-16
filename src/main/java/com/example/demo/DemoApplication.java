25ff39a39f4860e52e823226d56d8a5d6975eed1
mbodji <ibumbodji@gmail.com>
2023-01-16 21:33:15 +0100

    mise en de l'appli

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Scanner;
+
+@SpringBootApplication
+public class DemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+
+
+        System.out.println("=======Bonjour et bienvenue dans l'application Gestion de note ======");
+
+    }
+
+}
